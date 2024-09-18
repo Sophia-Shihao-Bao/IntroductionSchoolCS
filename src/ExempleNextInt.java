e7425b6a8b0609c74ac8fb26ce8d3d4535e8ba87
sophia <sophia.bao20@gmail.com>
2024-09-18 11:19:09 -0400

    Update in code

---
+++ src/ExempleNextInt.java
@@ -0,0 +1,5 @@
+public class ExempleNextInt {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Principal {
+    public static void mail(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Whats your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello"+name);
+        sc.close();
+    }
+}
